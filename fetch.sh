c2067e51d0d0dd687b57330ab44720985ab0ce36
Dixaba <dixaba@yandex.ru>
2019-03-05 19:35:35 +0300

    10-s

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in {1..100}
+for i in {1..10000}
 do
-    curl -s https://us-central1-crash-callstack-recipient.cloudfunctions.net/keycounter >> txt.txt
-    echo >> txt.txt
+    curl -s https://us-central1-crash-callstack-recipient.cloudfunctions.net/keycounter >> txt10.txt
+    echo >> txt10.txt
     echo $i
     sleep 8
 done
