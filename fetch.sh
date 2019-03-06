0730732e9c4417802f590b7b3c86d8bf67914c41
Dixaba <dixaba@yandex.ru>
2019-03-06 14:16:35 +0300

    ffgsd

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in {1..10000}
 do
-    curl -s https://us-central1-crash-callstack-recipient.cloudfunctions.net/keycounter >> txt17.txt
-    echo >> txt17.txt
+    curl -s https://us-central1-crash-callstack-recipient.cloudfunctions.net/keycounter >> txt18.txt
+    echo >> txt18.txt
     echo $i
     sleep 8
 done
